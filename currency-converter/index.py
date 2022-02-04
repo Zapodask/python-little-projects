4f30b50b0d249414da120631bb42346f89776454
Zapodask <joaopedroemerim@gmail.com>
2022-02-04 00:01:54 -0300

    currency converter v3

--- currency-converter/index.py
+++ currency-converter/index.py
@@ -1,22 +1,38 @@
+import json
 import requests
 import os
 
 
 class app:
+    options = {}
+
+    with open("list.json", "r", encoding="utf-8") as f:
+        options = json.loads(f.read())
+
     def __init__(self):
         os.system("cls")
 
         print("Conversor de moedas")
 
-        value = input("Valor: ")
+        v = input("Valor: ")
 
-        res = self.converter(value)
+        for k, _ in self.options.items():
+            print(k)
+
+        f = input("Escolha uma das moedas acima da qual quer converter: ")
+
+        for i in self.options[f]:
+            print(i)
+
+        t = input("Escolha uma das moedas acima para qual quer converter: ")
+
+        res = self.converter(float(v), f, t)
 
         print(res)
 
-    def converter(self, v):
-        res = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL").json()[
-            "USDBRL"
+    def converter(self, v, f, t):
+        res = requests.get(f"https://economia.awesomeapi.com.br/last/{f}-{t}").json()[
+            f"{f}{t}"
         ]
 
         cotação = (float(res["high"]) + float(res["low"])) / 2
